e4b07b6a286c29556b9e7bf0e885e7d792ae003e
Ricardo Ebbers <recl@cin.ufpe.br>
2020-11-21 15:54:33 -0300

    Fixed login flow to only appear when user is not logged in

--- app/src/main/java/br/ufpe/cin/nuota/MainActivity.kt
+++ app/src/main/java/br/ufpe/cin/nuota/MainActivity.kt
@@ -1,21 +1,40 @@
 package br.ufpe.cin.nuota
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import br.ufpe.cin.nuota.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var user = FirebaseAuth.getInstance().currentUser
+
+    companion object {
+        private const val RC_SIGN_IN = (Math.PI * 1000).toInt()
+        private val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("MainActivity", "logged in user: ${user?.displayName}")
+        if (user == null) {
+            startAuthActivity()
+        }
         super.onCreate(savedInstanceState)
+        startMainActivity()
+    }
 
+    private fun startMainActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -24,9 +43,22 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_scanner, R.id.navigation_sales))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_scanner, R.id.navigation_sales
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun startAuthActivity() {
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
     }
 }
