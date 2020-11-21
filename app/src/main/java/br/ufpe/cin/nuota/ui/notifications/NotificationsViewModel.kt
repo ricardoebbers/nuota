a5729e624adbe97fc90f4db389f2fb67f5e85011
Ricardo Ebbers <recl@cin.ufpe.br>
2020-11-21 14:10:02 -0300

    Revert project to native

---
+++ app/src/main/java/br/ufpe/cin/nuota/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package br.ufpe.cin.nuota.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
--- app/src/main/java/br/ufpe/cin/recl/nuota/NuotaApplication.kt
+++
@@ -1,7 +0,0 @@
-package br.ufpe.cin.recl.nuota
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class NuotaApplication : Application()
