7e2c8e6fec73b7ef32de0e292c1af045d16ffe60
joao <jpls2@cesar.org.br>
2020-11-24 23:02:46 -0300

    remove initialize app from download function

--- functions/src/download-invoice/download_invoice_handler.ts
+++ functions/src/download-invoice/download_invoice_handler.ts
@@ -1,7 +1,4 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-
-admin.initializeApp();
 
 export const downloadInvoiceHandler = functions.firestore
     .document('event_invoice_input/{invoiceId}')
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import { addUserToRegionHandler } from './add-user-to-region/add_user_to_region';
 import { addProductToUserHistoryHandler } from './add-product-to-user-history/add_product_to_user_history';
 import { addInvoiceHandler } from './mock-invoice-output/mock_invoice_output';
-import {downloadInvoiceHandler} from './download_invoice_handler';
+import {downloadInvoiceHandler} from './download-invoice/download_invoice_handler';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp();
