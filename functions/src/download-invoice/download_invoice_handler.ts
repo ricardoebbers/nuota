b074f6a4f515cd01a12b78287e62848f278e3166
joao <jpls2@cesar.org.br>
2020-11-25 21:06:12 -0300

    set JSDOM

--- functions/src/download-invoice/download_invoice_handler.ts
+++ functions/src/download-invoice/download_invoice_handler.ts
@@ -1,8 +1,22 @@
 import * as functions from 'firebase-functions';
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+(global as any).XMLHttpRequest = require('xhr2');
 
 export const downloadInvoiceHandler = functions.firestore
     .document('event_invoice_input/{invoiceId}')
     .onCreate(async (snap, _) => {
         const invoice = snap.data();
-        console.log(invoice);
+        const { url } = invoice;
+        const http = new XMLHttpRequest();
+        http.open('GET', url);
+        http.setRequestHeader('Accept', 'application/xml');
+
+        http.send();
+
+        http.onreadystatechange = (result) => {
+          const dom = new JSDOM(http.responseText);
+          const { window: { document }} = dom
+          console.log(document.location);
+        }
     });
