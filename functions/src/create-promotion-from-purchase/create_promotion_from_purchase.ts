0e8d9657767ed2fd29b95a69a315f77ef457d1f3
Ricardo Ebbers <recl@cin.ufpe.br>
2020-11-26 09:36:10 -0300

    Finished creating promotions from purchases

--- functions/src/create-promotion-from-purchase/create_promotion_from_purchase.ts
+++ functions/src/create-promotion-from-purchase/create_promotion_from_purchase.ts
@@ -1,29 +1,22 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { PurchasesInterface } from '../shared/models/invoice-model';
 
 export const createPromotionFromPurchaseHandler = functions.firestore
     .document('regions/{regionId}/products/{productId}/purchases/{purchaseId}')
     .onCreate(async (snap, context) => {
         const regionId = context.params.regionId;
-        const productId = context.params.productId;
-        const purchase = snap.data();
-        const productDoc = admin.firestore().collection('regions').doc(regionId).collection('products').doc(productId);
-        const current = (await productDoc.get()).data();
-        let sum: number;
-        let count: number;
-        if (current) {
-            sum = current.sum + purchase.unitValue;
-            count = current.count + 1;
-        } else {
-            sum = purchase.unitValue;
-            count = 1;
-        }
-        await productDoc.set({ sum, count }, { merge: true }); // update dos totalizadores para calcular a média
-
-        const average = sum / count;
-        const discountThreshold = 0.8; // 20% de desconto
-        if (purchase.unitValue / average <= discountThreshold) { // é uma promoção
-            const promotions = admin.firestore().collection('regions').doc(regionId).collection('promotions');
-            await promotions.add(purchase);
+        const purchase = snap.data() as PurchasesInterface;
+        const productDoc = (await snap.ref.parent.parent?.get())?.data();
+        if (productDoc) {
+            const sum = productDoc.sum;
+            const count = productDoc.count;
+            const average = sum / count;
+            const discountThreshold = 0.8; // 20% de desconto
+            if (purchase.product.unitValue / average <= discountThreshold) { // é uma promoção
+                const discount = (average - purchase.product.unitValue) / average;
+                const promotions = admin.firestore().collection('regions').doc(regionId).collection('promotions');
+                await promotions.add({ purchase, average, discount });
+            }
         }
     });
