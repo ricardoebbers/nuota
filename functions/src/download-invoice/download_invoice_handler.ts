07c53db4bd73b9b9eb2350c8e98c04d14c4bc16d
joao <jpls2@cesar.org.br>
2020-11-25 22:57:11 -0300

    Finish download invoice output

--- functions/src/download-invoice/download_invoice_handler.ts
+++ functions/src/download-invoice/download_invoice_handler.ts
@@ -1,22 +1,27 @@
 import * as functions from 'firebase-functions';
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+import * as admin from 'firebase-admin';
+import { parseString } from 'xml2js';
+import { InvoiceModel } from '../shared/models/invoice-model';
 (global as any).XMLHttpRequest = require('xhr2');
 
 export const downloadInvoiceHandler = functions.firestore
     .document('event_invoice_input/{invoiceId}')
     .onCreate(async (snap, _) => {
-        const invoice = snap.data();
-        const { url } = invoice;
+        const invoiceData = snap.data();
+        const { url } = invoiceData;
         const http = new XMLHttpRequest();
         http.open('GET', url);
         http.setRequestHeader('Accept', 'application/xml');
 
         http.send();
 
-        http.onreadystatechange = (result) => {
-          const dom = new JSDOM(http.responseText);
-          const { window: { document }} = dom
-          console.log(document.location);
+        http.onreadystatechange = async (response) => {
+          if(http.responseText !== '') {
+            parseString(http.responseText, async (err, result) => {
+              const invoice: InvoiceModel = InvoiceModel.fromObject(result);
+              const writeResult = admin.firestore().collection('event_invoice_output');
+              await writeResult.add(JSON.parse(JSON.stringify(invoice)));
+            });
+          }
         }
     });
